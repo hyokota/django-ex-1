438e9ef6486225b23cd92500dc433dfbf3d6499a
Hiroki Yokota(NSSOL) <yokota.hiroki.b3h@jp.nssol.nssmc.com>
2018-03-19 18:43:25 +0900

    django setting

--- manage.py
+++ manage.py
@@ -2,8 +2,8 @@
 import os
 import sys
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
     from django.core.management import execute_from_command_line
 
